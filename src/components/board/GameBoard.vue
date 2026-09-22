49f8a209e40702b2ff64f1863b9e2651ecea12a0
Gilomyk <gilomyk@gmail.com>
2026-09-22 18:47:41 +0200

    feat(ui): add board coordinates and layered border styling

--- src/components/board/GameBoard.vue
+++ src/components/board/GameBoard.vue
@@ -1,21 +1,61 @@
 <template>
   <div class="game-board-wrapper">
-    <div ref="boardElement" class="game-board">
-      <template v-for="(row, rowIdx) in board" :key="rowIdx">
-        <BoardCell
-          v-for="(cell, colIdx) in row"
-          :key="`${rowIdx}-${colIdx}`"
-          :row="cell.row"
-          :col="cell.col"
-          :isDark="cell.isDark"
-          :piece="cell.piece"
-          :isSelected="cell.isSelected"
-          :isTarget="cell.isTarget"
-          :move-offset="getMoveOffset(cell.row, cell.col)"
-          :move-duration-ms="props.moveDurationMs"
-          @click="(r, c) => emit('cell-click', r, c)"
-        />
-      </template>
+    <div class="board-coordinates-layout">
+      <div class="board-corner"></div>
+
+      <div class="board-files board-files--top">
+        <span v-for="label in fileLabels" :key="`top-${label}`">
+          {{ label }}
+        </span>
+      </div>
+
+      <div class="board-corner"></div>
+
+      <div class="board-ranks board-ranks--left">
+        <span v-for="label in rankLabels" :key="`left-${label}`">
+          {{ label }}
+        </span>
+      </div>
+
+      <div class="board-outer-white">
+        <div class="board-blue-ring">
+          <div class="board-inner-white">
+            <div ref="boardElement" class="game-board">
+              <template v-for="(row, rowIdx) in board" :key="rowIdx">
+                <BoardCell
+                  v-for="(cell, colIdx) in row"
+                  :key="`${rowIdx}-${colIdx}`"
+                  :row="cell.row"
+                  :col="cell.col"
+                  :isDark="cell.isDark"
+                  :piece="cell.piece"
+                  :isSelected="cell.isSelected"
+                  :isTarget="cell.isTarget"
+                  :move-offset="getMoveOffset(cell.row, cell.col)"
+                  :move-duration-ms="props.moveDurationMs"
+                  @click="(r, c) => emit('cell-click', r, c)"
+                />
+              </template>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="board-ranks board-ranks--right">
+        <span v-for="label in rankLabels" :key="`right-${label}`">
+          {{ label }}
+        </span>
+      </div>
+
+      <div class="board-corner"></div>
+
+      <div class="board-files board-files--bottom">
+        <span v-for="label in fileLabels" :key="`bottom-${label}`">
+          {{ label }}
+        </span>
+      </div>
+
+      <div class="board-corner"></div>
     </div>
   </div>
 </template>
@@ -26,6 +66,8 @@ import BoardCell from '@/components/board/BoardCell.vue'
 import type { CellView, PieceMoveView } from '@/types/board-view'
 
 const boardElement = ref<HTMLElement | null>(null)
+const fileLabels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+const rankLabels = ['9', '8', '7', '6', '5', '4', '3', '2', '1']
 
 const props = defineProps<{
   board: CellView[][]
@@ -82,6 +124,84 @@ defineExpose({
     inset 0 0 0 1px rgba(255, 255, 255, 0.08);
 }
 
+.board-coordinates-layout {
+  display: grid;
+  grid-template-columns: 18px auto 18px;
+  grid-template-rows: 18px auto 18px;
+  gap: 4px;
+}
+
+.board-outer-white {
+  grid-column: 2;
+  grid-row: 2;
+
+  padding: 2px;
+  background: #ffffff;
+}
+
+.board-blue-ring {
+  padding: 2px;
+  background: var(--color-board-bg);
+}
+
+.board-inner-white {
+  padding: 4px;
+  background: #ffffff;
+}
+
+.board-ranks {
+  display: grid;
+  grid-template-rows: repeat(9, 1fr);
+  align-items: center;
+  justify-items: center;
+}
+
+.board-ranks--left {
+  grid-column: 1;
+  grid-row: 2;
+}
+
+.board-ranks--right {
+  grid-column: 3;
+  grid-row: 2;
+}
+
+.board-files {
+  display: grid;
+  grid-template-columns: repeat(9, 1fr);
+  align-items: center;
+  justify-items: center;
+}
+
+.board-files--top {
+  grid-column: 2;
+  grid-row: 1;
+}
+
+.board-files--bottom {
+  grid-column: 2;
+  grid-row: 3;
+}
+
+.board-ranks span,
+.board-files span {
+  color: #ffffff;
+  font-size: 0.7rem;
+  font-weight: 700;
+  line-height: 1;
+  user-select: none;
+}
+
+.board-files {
+  width: 97.5%;
+  justify-self: center;
+}
+
+.board-ranks {
+  height: 97.5%;
+  align-self: center;
+}
+
 .game-board {
   display: grid;
   grid-template-columns: repeat(9, 1fr);
@@ -101,6 +221,29 @@ defineExpose({
   .game-board-wrapper {
     padding: 5px;
     border-width: 2px;
+  }
+
+  .board-coordinates-layout {
+    grid-template-columns: 14px auto 14px;
+    grid-template-rows: 14px auto 14px;
+    gap: 3px;
+  }
+
+  .board-outer-white {
+    padding: 3px;
+  }
+
+  .board-blue-ring {
+    padding: 2px;
+  }
+
+  .board-inner-white {
+    padding: 1px;
+  }
+
+  .board-ranks span,
+  .board-files span {
+    font-size: 0.58rem;
   }
 }
 </style>
